5bf92fdb53890f0a4bdb0705b51e096aa83a9a17
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-08-24 00:18:05 +0900

    [FEAT] 유저 관련 예외처리
    회원가입, 로그인, 사용자 정보 수정 관련 예외처리

--- src/main/java/com/bit/shoppingmall/controller/UserController.java
+++ src/main/java/com/bit/shoppingmall/controller/UserController.java
@@ -3,15 +3,24 @@ package com.bit.shoppingmall.controller;
 import com.bit.shoppingmall.dto.LoginRequest;
 import com.bit.shoppingmall.dto.LoginResponse;
 import com.bit.shoppingmall.dto.SignUpRequest;
+import com.bit.shoppingmall.exception.DuplicateKeyException;
+import com.bit.shoppingmall.exception.NoSuchDataException;
 import com.bit.shoppingmall.global.LabelFormat;
 import com.bit.shoppingmall.service.UserService;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class UserController extends HttpServlet {
 
@@ -48,30 +57,42 @@ public class UserController extends HttpServlet {
 
     }
 
-    // post login
-    private void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("로그인");
-        LoginRequest loginRequest = new LoginRequest(request.getParameter("email"), request.getParameter("password"));
-        LoginResponse loginResponse = userService.login(loginRequest);
+    // login post
+    private void login(HttpServletRequest request, HttpServletResponse response) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, ServletException, IOException {
 
-        request.getSession().setAttribute("login_user", loginResponse.getLoginUser());
-        request.getSession().setAttribute("grade", loginResponse.getGrade());
-        request.getSession().setAttribute("discount_rate", loginResponse.getDiscountRate());
+        try {
+            LoginRequest loginRequest = new LoginRequest(request.getParameter("email"), request.getParameter("password"));
+            LoginResponse loginResponse = null;
 
-        System.out.println(loginResponse);
-        RequestDispatcher rd = request.getRequestDispatcher(LabelFormat.PREFIX.label() + "myPage" + LabelFormat.SUFFIX.label());
-        rd.forward(request, response);
+            loginResponse = userService.login(loginRequest);
+            request.getSession().setAttribute("login_user", loginResponse.getLoginUser());
+            request.getSession().setAttribute("grade", loginResponse.getGrade());
+            request.getSession().setAttribute("discount_rate", loginResponse.getDiscountRate());
+
+            RequestDispatcher rd = request.getRequestDispatcher(LabelFormat.PREFIX.label() + "myPage" + LabelFormat.SUFFIX.label());
+            rd.forward(request, response);
+
+        } catch (NoSuchDataException e) {
+            request.setAttribute("errorMsg", e.getMessage());
+            RequestDispatcher dispatcher = request.getRequestDispatcher(LabelFormat.PREFIX.label() + "user" + LabelFormat.SUFFIX.label());
+            dispatcher.forward(request, response);
+        }
 
     }
 
-    // post sign-up
-    private void signUp(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    // sign-up post
+    private void signUp(HttpServletRequest request, HttpServletResponse response) throws InvalidAlgorithmParameterException, IOException, IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException, ServletException {
 
-        String address = request.getParameter("address") + request.getParameter("address_detail");
-        SignUpRequest signUpRequest = new SignUpRequest(request.getParameter("email"), request.getParameter("password"), request.getParameter("username"), request.getParameter("phone_number"), address);
+        try {
+            String address = request.getParameter("address") + request.getParameter("address_detail");
+            SignUpRequest signUpRequest = new SignUpRequest(request.getParameter("email"), request.getParameter("password"), request.getParameter("username"), request.getParameter("phone_number"), address);
 
-        userService.signUp(signUpRequest);
-        response.sendRedirect("../home");
+            userService.signUp(signUpRequest);
+            response.sendRedirect("../home");
+        } catch (DuplicateKeyException e) {
+            request.setAttribute("errorMsg", e.getMessage());
+            RequestDispatcher dispatcher = request.getRequestDispatcher(LabelFormat.PREFIX.label() + "user" + LabelFormat.SUFFIX.label());
+            dispatcher.forward(request, response);
+        }
     }
-
 }
---
+++ src/main/java/com/bit/shoppingmall/validation/UserValidation.java
@@ -0,0 +1,56 @@
+package com.bit.shoppingmall.validation;
+
+import com.bit.shoppingmall.global.Validation;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 회원가입 post, 사용자 정보 수정(비밀번호), 사용자 정보 수정(폰번호, 주소) 확인
+public class UserValidation extends HttpServlet {
+
+    Validation validation = new Validation();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+        String uri = request.getRequestURI();
+        String path = uri.substring(0, uri.lastIndexOf("."));
+
+        if (path.equals("/user-validation/sign-up")) {
+            signUpFormValidation(request, response);
+        } else if (path.equals("/user-validation/my-page-info/pass")) { //사용자 정보 수정
+            passUpdateFormValidation(request, response);
+        }
+    }
+
+    public void signUpFormValidation(HttpServletRequest request, HttpServletResponse response) {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String userName = request.getParameter("username");
+        String phoneNumber = request.getParameter("phone_number");
+        String address = request.getParameter("address") + request.getParameter("address_detail");
+
+        // 사이즈, NotNull 검사
+        validation.validateString("이메일", email, 127);
+        validation.validateString("비밀번호", password, 127);
+        validation.validateString("유저이름", userName, 127);
+        validation.validateString("휴대폰 번호", phoneNumber, 127);
+        validation.validateString("주소", address, 127);
+
+        // 이메일, 패스워드 양식 검사
+        validation.validateEmail(email);
+        validation.validatePassword(password);
+
+    }
+
+    public void passUpdateFormValidation(HttpServletRequest request, HttpServletResponse response) {
+
+        String password = request.getParameter("update_password");
+        // 사이즈, NotNull 검사
+        validation.validateString("변경할 비밀번호", password, 127);
+        // 이메일, 패스워드 양식 검사
+        validation.validatePassword(password);
+    }
+}
