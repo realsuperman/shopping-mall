763c53ca81170c7d81bbd75706eb12381915dbbc
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-08-25 10:18:08 +0900

    [FEAT] - 카카오 페이 취소 관련 API 추가 - 화면 관련 예외 처리 추가

--- src/main/java/com/bit/shoppingmall/global/KakaoPayProcess.java
+++ src/main/java/com/bit/shoppingmall/global/KakaoPayProcess.java
@@ -1,5 +1,6 @@
 package com.bit.shoppingmall.global;
 
+import com.bit.shoppingmall.vo.KakaoPayCancelCancelVO;
 import com.bit.shoppingmall.vo.KakaoPayVO;
 
 import java.io.DataOutputStream;
@@ -22,6 +23,26 @@ public class KakaoPayProcess {
                 + "&partner_order_id=" + kakaoPayVO.getPartnerOrderId()
                 + "&partner_user_id=" + kakaoPayVO.getPartnerUserId()
                 + "&pg_token=" + kakaoPayVO.getPgToken();
+
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeBytes(payloadData);
+
+        return connection.getResponseCode();
+    }
+
+    public int cancel(KakaoPayCancelCancelVO kakaoPayCancelCancelVO) throws IOException {
+        URL url = new URL("https://kapi.kakao.com/v1/payment/approve");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "KakaoAK 1c2665d0fbd94a38d95ac129fa3d165a");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        connection.setDoOutput(true);
+
+        String payloadData = "cid=" + kakaoPayCancelCancelVO.getCid()
+                + "&tid=" + kakaoPayCancelCancelVO.getTid()
+                + "&cancel_amount=" + kakaoPayCancelCancelVO.getCancelAmount()
+                + "&cancel_tax_free_amount=" + kakaoPayCancelCancelVO.getCancelTaxFreeAmount();
 
         DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
         dos.writeBytes(payloadData);
